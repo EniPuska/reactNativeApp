b8bd3a963b023d8cb1fdfdeabb97fcd63401e930
Eni Puska <eni.puska@gmail.com>
2019-07-29 23:04:13 +0300

    ...

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Input, Button } from 'react-native-elements';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    username: '',
+    password:''
+  }
+
+  inputChangeHandler = (value) => {
+    this.setState({username:value});
+  }
+
+  loginAction = () => {
+    console.log(this.state.username);
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Username</Text>
+        <Input 
+          value = {this.state.username}
+          onChangeText = {this.inputChangeHandler}
+        />
+        <Text>Password</Text>
+        <Input 
+          value = {this.state.password}
+          onChangeText = {this.inputChangeHandler}
+        />
+        <Button
+          title = 'Login'
+          onPress = {this.loginAction}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
