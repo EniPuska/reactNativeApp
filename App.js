a6bad91539e68e32e7377199565f31e7e84fadcd
Eni Puska <eni.puska@gmail.com>
2019-08-01 17:52:32 +0300

    ...

--- App.js
+++ App.js
@@ -1,49 +1,14 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Input, Button } from 'react-native-elements';
-
-export default class App extends Component {
-  state = {
-    username: '',
-    password:'',
-    loginDetails:{}
-  }
-
-  inputChangeHandler = (value,type) => {
-    if(type == 1) {
-      this.setState({username:value});
-    } else {
-      this.setState({password:value});
-    }
-    
-  }
-
-  loginAction = () => {
-    this.state.loginDetails.username = this.state.username;
-    this.state.loginDetails.password = this.state.password;
-    console.log(this.state.loginDetails);
-  }
-  
+import ToDoApp from './src/ToDoApp';
+import store from './src/store';
+import { Provider } from 'react-redux';
+export default class App extends Component{
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Username</Text>
-        <Input 
-          value = {this.state.username}
-          name = 'username'
-          onChangeText = {(value) => this.inputChangeHandler(value,1)}
-        />
-        <Text>Password</Text>
-        <Input 
-          value = {this.state.password}
-          name = 'password'
-          onChangeText = {(value) => this.inputChangeHandler(value,2)}
-        />
-        <Button
-          title = 'Login'
-          onPress = {this.loginAction}
-        />
-      </View>
+      <Provider store = {store}>
+        <ToDoApp></ToDoApp>
+      </Provider>
     );
   }
 }
